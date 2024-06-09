1d7a8a4530d41dc653551099c35ebe2fab7b47b2
vasalerm1401 <vasalerm008@gmail.com>
2024-06-09 22:53:36 +0300

    Четвертая версия

--- src/main/java/ru/practicum/shareit/exception/NotUniqueEmailException.java
+++
@@ -1,6 +0,0 @@
-package ru.practicum.shareit.exception;
-public class NotUniqueEmailException extends RuntimeException {
-    public NotUniqueEmailException(String message) {
-        super(message);
-    }
-}
