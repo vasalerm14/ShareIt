699f61a4b5bc46d9c1338ab299e6fdb68930cd85
vasalerm1401 <vasalerm008@gmail.com>
2024-06-13 21:22:24 +0300

    Вторая версия

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserGController.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDtoRequest;
+import javax.validation.Valid;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserGController {
+    private final UserClient userClient;
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        log.info("GET / users");
+        return userClient.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUserById(@PathVariable int userId) {
+        log.info("GET / users / {}", userId);
+        return userClient.getUserById(userId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveNewUser(@Valid @RequestBody UserDtoRequest userDto) {
+        log.info("POST / users / {} / {}", userDto.getName(), userDto.getEmail());
+        return userClient.saveNewUser(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable int userId,
+                                             @RequestBody UserDtoRequest userDto) {
+        log.info("PATCH / users / {}", userId);
+        return userClient.updateUser(userId, userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable int userId) {
+        log.info("DELETE / users / {}", userId);
+        return userClient.deleteUser(userId);
+    }
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDtoRequest.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDtoRequest {
+
+    private int id;
+    @NotBlank
+    private String name;
+    @NotEmpty
+    @Email
+    private String email;
+}
